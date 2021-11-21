a3bc5582f6120915571ae32ae33d8f7e029b6a44
Jhon salazar <jhonzallazar@gmail.com>
2021-11-20 22:00:06 -0500

    Se definieron las funciones para poder mover las barras del juego

--- index.js
+++ index.js
@@ -26,14 +26,15 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
+        this.speed = 10;
     }
     
     self.Bar.prototype = {
         down: function(){
-
+            this.y += this.speed;
         },
         up: function(){
-
+            this.y -= this.speed;
         }
     }
     
@@ -71,7 +72,16 @@
 
 }());
 
-window.addEventListener("load",main);
+document.addEventListener("keydown",function(ev){
+    if(ev.keyCode ==38){
+        bar.up;
+    }
+    else if(ev.keyCode == 40){
+        bar.down;
+    }
+});
+
+self.addEventListener("load",main);
 
 function main(){
     let board = new Board(800,400);
